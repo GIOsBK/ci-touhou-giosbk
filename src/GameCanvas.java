5f77bcee0a0ace71f4479e00f5c86eac49a4eb30
GIOsBK <hatran10091996@gmail.com>
2017-10-11 20:46:18 +0700

    added palyer, background drawing added size fixing

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,7 +1,32 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameCanvas extends JPanel {
+    BufferedImage background;
+    BufferedImage player;
+
     public GameCanvas(){
+        //1.Load background
+        try {
+            background = ImageIO.read(new File("assets/images/background/0.png"));
+            player = ImageIO.read(new File("assets/images/players/straight/0.png"));
+        } catch (IOException e) {
+            e.printStackTrace();// lich su goi ham
+        }
+    }
+    //2.draw background
+
+    @Override
+    protected void paintComponent(Graphics g) {
+       g.drawImage(background, 0, 0,null);
+       g.drawImage(player,180, 500,null);
 
     }
+
+    //3.load player
+
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,14 +1,58 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameWindow extends JFrame {
     GameCanvas canvas;
 
     public GameWindow(){
         this.setSize(800,600);
-        this.setVisible(true);
-        this.canvas = new GameCanvas();
-        this.setContentPane(canvas);
 
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
+        //this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+               System.exit(0);
+
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+        this.setResizable(false);
+        this.canvas.setVisible(true);
+        this.setVisible(true);
     }
+
 }
